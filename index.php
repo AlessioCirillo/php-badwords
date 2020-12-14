f96b87252faea67bad9331a221fc18bcaba47ab4
alessio <alessio.cirillo.1995@gmail.com>
2020-12-14 17:05:50 +0100

    all done

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <?php
+    // variabile
+    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sed quos suscipit, odit quia distinctio at accusantium corrupti nemo nesciunt sint dolores iste. Exercitationem velit facere, ex dignissimos id cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sed quos suscipit, odit quia distinctio at accusantium corrupti nemo nesciunt sint dolores iste. Exercitationem velit facere, ex dignissimos id cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sed quos suscipit, odit quia distinctio at accusantium corrupti nemo nesciunt sint dolores iste. Exercitationem velit facere, ex dignissimos id cupiditate';
+    $length = strlen($text);
+    $badwords = $_GET['badwords'];
+    $change_word = str_replace($badwords, '***', $text);
+    ?>
+    
+    <h1>Paragrafo</h1>
+    <p><?php echo $text ?></p>
+
+    <h1>Lunghezza paragrafo</h1>
+    <p> <?php echo $length ?></p>
+
+    <h2><?php echo $change_word ?></h2>
+</body>
+</html>
